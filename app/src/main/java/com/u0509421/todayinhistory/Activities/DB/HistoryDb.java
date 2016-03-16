cfc94f1f17e8efcbcc42a440d1a658a77902faf4
Terry Lu <u0509421@gmail.com>
2016-03-17 02:38:35 +0800

    third commit

---
+++ app/src/main/java/com/u0509421/todayinhistory/Activities/DB/HistoryDb.java
@@ -0,0 +1,36 @@
+package com.u0509421.todayinhistory.Activities.DB;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Terry on 17/3/16.
+ */
+public class HistoryDb extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "HistoryDb";
+
+    public HistoryDb(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String sql = "CREATE TABLE history ( " +
+                "_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "title TEXT not null unique, "+
+                "date TEXT, "+
+                "content TEXT )";
+
+        db.execSQL(sql);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
